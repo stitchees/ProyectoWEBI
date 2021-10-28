10f07fac6f4d0943cb484cb034cd650ce15f90aa
cosentinowevdev <rodrigocosentino@outlook.com>
2021-10-28 07:45:06 -0300

    fix repo

--- src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioPlanViaje.java
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioPlanViaje.java
@@ -1,5 +1,12 @@
 package ar.edu.unlam.tallerweb1.repositorios;
 
+import java.util.List;
+
+import ar.edu.unlam.tallerweb1.modelo.PlanViaje;
+
 public interface RepositorioPlanViaje {
 
+	static List<PlanViaje> consultarPlanesDeViaje(Integer presupuesto, String destino);
+
+	
 }
